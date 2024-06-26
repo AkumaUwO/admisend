7d4832fe118940a21a6ee5ef9f34e668d2979b27
Ale035 <akumauwo2021@gmail.com>
2024-06-26 11:44:33 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET admin page. */
+router.get('/admin', function(req, res, next) {
+  res.render('admin');
+});
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+module.exports = router;
