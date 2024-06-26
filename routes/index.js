59ebbc1f7df5e1e55152389cfb837c94a5ef3ad2
Ale035 <akumauwo2021@gmail.com>
2024-06-26 12:56:40 -0400

    add corrections to db connection

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,32 @@
 var express = require('express');
 var router = express.Router();
-var connection = require('../modules/connection');
+const mysql = require('mysql');
+
+/*Creando conexion a la base de datos*/
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'admisend'
+});
+
+
+/*Iniciando conexion con la base de datos*/
+function connect(){
+  connection.connect((err) => {
+    if (err) {
+        console.error('Error al conectar:', err);
+        return;
+    }
+    console.log('Conexión exitosa');
+  });
+}
+
+/*Finalizando conexion con la base de datos*/
+function disconnect(){
+  connection.end();
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -15,6 +41,20 @@ router.get('/admin', function(req, res, next) {
 /* GET login page. */
 router.get('/login', function(req, res, next) {
   res.render('login');
+});
+
+/* Comprobar y validar inicio de sesión al enviar el formulario */
+router.post('/login', (req, res, next) => {
+  connect();
+  connection.query('SELECT * FROM usuarios', (error, results, fields) => {
+    if (error) {
+        console.error('Error en la consulta:', error);
+        return;
+    }
+    console.log('Resultados:', results);
+  });
+  disconnect();
+
 });
 
 /* GET register page. */
